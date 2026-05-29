b3159cf8cae144bcec5c59e0ba6dff3c2ac57cb8
Raushan Raj <raushanraj3107@gmail.com>
2026-05-29 18:29:26 +0530

    Random password generator

---
+++ 09_Random_password_generator/index.js
@@ -0,0 +1,24 @@
+function generatePassword(length,includelowercase,includeuppercase,includenumbers,includesymbols)
+{
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+let allowedChars="";
+let password="";
+allowedChars +=includelowercase?lowercaseChars:"";
+allowedChars +=includeuppercase?uppercaseChars:"";
+allowedChars +=includenumbers?numberChars:"";
+allowedChars +=includesymbols?symbolChars:"";
+
+if(length<=0){
+    return `(password length must be atleast 1)`;
+}
+
+//check ek bhi charactes allow hai ya nhi
+if(allowedChars.length===0){
+    return `(At least 1 set of characters need to be selected)`;
+}
+}
+
